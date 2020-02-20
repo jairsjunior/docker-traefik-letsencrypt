1d16738c4c1fefa9a0fe495796eb0a653dc6b777
Jair de Souza Junior <detento112@gmail.com>
2020-02-20 09:35:16 -0300

    Add of environment to redirect https

--- startup.sh
+++ startup.sh
@@ -9,13 +9,23 @@ if [ -z $SWARM_MODE ]; then
     SWARM_MODE=false
 fi
 
+if [ -z $HTTPS_REDIRECT ]; then
+    if [ "$HTTPS_REDIRECT" = true ]; then
+        ENTRYPOINTS='Name:http Address::80 Redirect.EntryPoint:https'
+    else
+        ENTRYPOINTS='Name:http Address::80'
+    fi
+else
+    ENTRYPOINTS='Name:http Address::80'
+fi
+
 touch /acme.json
 chmod 600 acme.json
 
 traefik --api  --docker --docker.watch --docker.domain=docker.localhost --docker.swarmMode=$SWARM_MODE \
 --ping --ping.entryPoint=http \
 --metrics.prometheus \
---entrypoints='Name:http Address::80 Redirect.EntryPoint:https' \
+--entrypoints=$ENTRYPOINTS \
 --entryPoints='Name:https Address::443 TLS' \
 --acme \
 --acme.acmelogging \
@@ -25,4 +35,4 @@ traefik --api  --docker --docker.watch --docker.domain=docker.localhost --docker
 --acme.onhostrule  --acme.httpchallenge \
 --acme.httpchallenge.entrypoint=http \
 --retry \
---loglevel=DEBUG
+--loglevel=INFO
